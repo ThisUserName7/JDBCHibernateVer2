6810e919b631b68f6b80cc17cc660dad771aae07
Ivan Zubkov <115741332+ThisUserName7@users.noreply.github.com>
2024-06-13 19:26:49 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,7 +8,6 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao userDao = new UserDaoJDBCImpl();
     private final UserDao userDaoHibernate = new UserDaoHibernateImpl();
     
     @Override
